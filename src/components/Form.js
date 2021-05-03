b9050e371ff0a5786b6990a96e5c0ceb4135a0ca
Adeyemi juwon <samju7778@gmail.com>
2021-05-03 12:14:34 +0100

    home

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [name, setName]=useState('')
+   const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+     const { data } = await Axios.post('https://foodapisamju.herokuapp.com/feedback', {name, email, message });
+     localStorage.setItem('message', JSON.stringify(data));
+     setName('')
+     setEmail('')
+     setMessage('')
+  }
   return (
     <div className="contact" id="contact">
       <h1>CONTACT</h1>
       <p>Have a question or want to work together?</p>
-      <form id="form">
-        <input type="text" id="name" placeholder="Name" />
+      <form id="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <span className="error"></span>
         <br />
-        <input type="email" id="email" placeholder="Enter Email " />
+        <input
+          type="email"
+          id="email"
+          value={email}
+          placeholder="Enter Email "
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <span className="error"></span>
         <br />
         <textarea
           name="message"
           id="message"
           placeholder="Your Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
         <span className="error"></span>
         <br />
