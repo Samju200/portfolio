7fe7c5c906961827b67c93addfc015ea9f80b6f7
Adeyemi juwon <samju7778@gmail.com>
2022-09-01 14:25:30 +0100

    skills edited

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,20 +7,12 @@ function Skills() {
         <h1>Skills</h1>
         <ul className="skill-list">
           <li> GIT </li>
-          <li> WIREFRAME</li>
           <li> HTML/CSS</li>
           <li>JAVASCRIPT</li>
           <li>MYSQL</li>
-          <li>PHP</li>
           <li>Node.js(Express)</li>
           <li>React</li>
           <li>Vue</li>
-          <li>Tailwind css</li>
-          <li>Next/Next-auth</li>
-          <li>MongoDB</li>
-          <li>Firebase</li>
-          <li>Product Design</li>
-          <li>Python(beginner level)</li>
         </ul>
       </div>
     </div>
