64961e58890f263f02b1481135c19c31870826b8
samju200 <samju7778@gmail.com>
2023-04-17 14:44:12 +0100

    add link

--- src/components/data.js
+++ src/components/data.js
@@ -1,129 +1,137 @@
-import Simple from './img/simple.png';
-import Dom from './img/dom1.png';
-import Currency from './img/currency.png';
-import LinkedLin from './img/linkedlin-clone.png';
-import Template from './img/template.png';
-import Todo from './img/todo-react.png';
-import Gmail from './img/gmail-clone.png';
-import Shop from './img/shop.png';
-import Netflix from './img/netflix.png';
-import Spotify from './img/sportify.png';
-import Construction from './img/construction.png';
-import Instagram from './img/instagram.png';
-import FoodBag from './img/FoodBag.PNG';
-import Design from './img/Onboarding.png';
-import Slide1 from './slideName/slide_1.jpg';
-import Slide2 from './slideName/slide_2.jpg';
-import Slide3 from './slideName/slide_3.jpg';
-import Slide4 from './slideName/slide_4.jpg';
-import Slide5 from './slideName/slide_5.jpg';
-import Slide6 from './slideName/slide_6.jpg';
-import Slide7 from './slideName/slide_7.jpg';
-import Slide8 from './slideName/slide_8.jpg';
+import Simple from "./img/simple.png";
+import Dom from "./img/dom1.png";
+import Currency from "./img/currency.png";
+import LinkedLin from "./img/linkedlin-clone.png";
+import Template from "./img/template.png";
+import Todo from "./img/todo-react.png";
+import Gmail from "./img/gmail-clone.png";
+import Shop from "./img/shop.png";
+import Netflix from "./img/netflix.png";
+import Spotify from "./img/sportify.png";
+import Construction from "./img/construction.png";
+import Deliveroo from "./img/Deliveroo-clone.png";
+import Instagram from "./img/instagram.png";
+import FoodBag from "./img/FoodBag.PNG";
+import Design from "./img/Onboarding.png";
+import Slide1 from "./slideName/slide_1.jpg";
+import Slide2 from "./slideName/slide_2.jpg";
+import Slide3 from "./slideName/slide_3.jpg";
+import Slide4 from "./slideName/slide_4.jpg";
+import Slide5 from "./slideName/slide_5.jpg";
+import Slide6 from "./slideName/slide_6.jpg";
+import Slide7 from "./slideName/slide_7.jpg";
+import Slide8 from "./slideName/slide_8.jpg";
 
 export const portfolio = [
   {
+    id: 1234,
+    title: "Deliveroo React_native App",
+    image: Deliveroo,
+    link: "",
+    githublink: "https://github.com/Samju200/deliveroo-clone",
+  },
+  {
     id: 123,
-    title: 'FoodBag',
+    title: "FoodBag",
     image: FoodBag,
-    link: 'https://foodbag-three.vercel.app/',
-    githublink: 'https://github.com/Samju200/foodbag',
+    link: "https://foodbag-three.vercel.app/",
+    githublink: "https://github.com/Samju200/foodbag",
   },
   {
     id: 1,
-    title: 'Netflix Clone',
+    title: "Netflix Clone",
     image: Netflix,
-    link: 'https://netflix-clone-37df8.web.app/',
-    githublink: 'https://github.com/Samju200/netflix-clone',
+    link: "https://netflix-clone-37df8.web.app/",
+    githublink: "https://github.com/Samju200/netflix-clone",
   },
   {
     id: 2,
-    title: 'Construction',
+    title: "Construction",
     image: Construction,
-    link: 'https://construction-sigma.vercel.app/',
-    githublink: 'https://github.com/Samju200/construction',
+    link: "https://construction-sigma.vercel.app/",
+    githublink: "https://github.com/Samju200/construction",
   },
   {
     id: 3,
-    title: 'Template',
+    title: "Template",
     image: Template,
-    link: 'https://samju200.github.io/formplus-template-task/',
-    githublink: 'https://github.com/Samju200/formplus-template-task',
+    link: "https://samju200.github.io/formplus-template-task/",
+    githublink: "https://github.com/Samju200/formplus-template-task",
   },
   {
     id: 4,
-    title: 'Instagram Clone',
+    title: "Instagram Clone",
     image: Instagram,
-    link: 'https://instagram-next-kappa.vercel.app/',
-    githublink: 'https://github.com/Samju200/instagram-next',
+    link: "https://instagram-next-kappa.vercel.app/",
+    githublink: "https://github.com/Samju200/instagram-next",
   },
   {
     id: 5,
-    title: 'Shop With Samju',
+    title: "Shop With Samju",
     image: Shop,
-    link: 'https://samju200.github.io/my-online-shop',
-    githublink: 'https://github.com/Samju200/my-online-shop',
+    link: "https://samju200.github.io/my-online-shop",
+    githublink: "https://github.com/Samju200/my-online-shop",
   },
   {
     id: 6,
-    title: 'Spotify Clone',
+    title: "Spotify Clone",
     image: Spotify,
-    link: 'https://sportify-lake.vercel.app/',
-    githublink: 'https://github.com/Samju200/sportify',
+    link: "https://sportify-lake.vercel.app/",
+    githublink: "https://github.com/Samju200/sportify",
   },
   {
     id: 7,
-    title: 'Gmail Clone',
+    title: "Gmail Clone",
     image: Gmail,
-    link: 'https://clone-6faf4.web.app/',
-    githublink: 'https://github.com/Samju200/gmail-clone',
+    link: "https://clone-6faf4.web.app/",
+    githublink: "https://github.com/Samju200/gmail-clone",
   },
   {
     id: 8,
-    title: 'Currency Exchange',
+    title: "Currency Exchange",
     image: Currency,
-    link: 'https://samju200.github.io/samju/currency/index.html',
-    githublink: 'https://github.com/Samju200/samju/tree/master/currency',
+    link: "https://samju200.github.io/samju/currency/index.html",
+    githublink: "https://github.com/Samju200/samju/tree/master/currency",
   },
   {
     id: 9,
-    title: 'LinkedlIn Clone',
+    title: "LinkedlIn Clone",
     image: LinkedLin,
-    link: 'https://linkedin-clone-yt-4878b.web.app/',
-    githublink: 'https://github.com/Samju200',
+    link: "https://linkedin-clone-yt-4878b.web.app/",
+    githublink: "https://github.com/Samju200",
   },
 
   {
     id: 10,
-    title: 'Product Design',
+    title: "Product Design",
     image: Design,
-    link: 'https://www.behance.net/gallery/116553949/African-food-restaurant',
+    link: "https://www.behance.net/gallery/116553949/African-food-restaurant",
     githublink:
-      'https://www.figma.com/file/ocdynE5cpgsMdSnwAVUbIY/Team-2--AFI-(Copy)?node-id=556%3A19801',
+      "https://www.figma.com/file/ocdynE5cpgsMdSnwAVUbIY/Team-2--AFI-(Copy)?node-id=556%3A19801",
   },
 
   {
     id: 12,
-    title: 'Advance Todo Dragging and drop',
+    title: "Advance Todo Dragging and drop",
     image: Todo,
-    link: 'https://samju200.github.io/todo/',
-    githublink: 'https://github.com/Samju200/todo',
+    link: "https://samju200.github.io/todo/",
+    githublink: "https://github.com/Samju200/todo",
   },
 
   {
     id: 13,
-    title: 'E-Commerce',
+    title: "E-Commerce",
     image: Simple,
-    link: 'https://samju200.github.io/samju/simple/product.html',
-    githublink: 'https://github.com/Samju200/samju/tree/master/simple',
+    link: "https://samju200.github.io/samju/simple/product.html",
+    githublink: "https://github.com/Samju200/samju/tree/master/simple",
   },
 
   {
     id: 14,
-    title: 'Dom',
+    title: "Dom",
     image: Dom,
-    link: 'https://samju200.github.io/DOM/dom1/dom.html',
-    githublink: 'https://github.com/Samju200/DOM/tree/master/dom1',
+    link: "https://samju200.github.io/DOM/dom1/dom.html",
+    githublink: "https://github.com/Samju200/DOM/tree/master/dom1",
   },
 ];
 export const slideImage = [
