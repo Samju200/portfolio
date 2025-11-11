11f49f081ef4320a4521587fdc70662995e68231
samju200 <samju7778@gmail.com>
2025-11-11 14:14:58 +0100

    this

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,25 +1,27 @@
 import React from "react";
 
 function Skills() {
-    return (
-        <div>
-            <div className="skill" id="skills">
-                <h1>Skills</h1>
-                <ul className="skill-list">
-                    <li> GIT </li>
-                    <li> HTML/CSS</li>
-                    <li>JAVASCRIPT/TYPESCRIPT</li>
-                    <li>MYSQL/MongoDB/Firebase</li>
-                    <li>Node.js(Express)</li>
-                    <li>React/Next</li>
-                    <li>React-native</li>
-                    <li>Vue</li>
-                    <li>PHP</li>
-                    <li>Java Springboot</li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="skill" id="skills">
+        <h1>Skills</h1>
+        <ul className="skill-list">
+          <li> GIT </li>
+          <li> HTML/CSS</li>
+          <li>JAVASCRIPT/TYPESCRIPT</li>
+          <li>MYSQL/MongoDB/Firebase/PostgreSQL</li>
+          <li>Node.js(ExpressJS)</li>
+          <li>Node.js(NestJS)</li>
+          <li>React/Next</li>
+          <li>Angular</li>
+          <li>React-native</li>
+          <li>Vue</li>
+          <li>PHP( Laravel )</li>
+          <li>Java Springboot</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
